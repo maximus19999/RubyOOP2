6564eb56abc67d3df2617971121d980c813cd433
Max Anderson <maxwell.anderson@griffithuni.edu.au>
2015-09-25 00:01:25 +1000

    practice3

--- terrain/terrain.rb
+++ terrain/terrain.rb
@@ -5,8 +5,13 @@ class Terrain
   def print_word2
     puts "timmy222"
   end
+  def print_word2
+    puts "timmy222"
+  end
 end
 
 tim = Terrain.new
+tim.print_word
+tim.print_word2
 tim.print_word
 tim.print_word2
