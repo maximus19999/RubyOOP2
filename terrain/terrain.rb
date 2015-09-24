75d06e56f22d861c21c0e4b080ed1f50687865e5
Max Anderson <maxwell.anderson@griffithuni.edu.au>
2015-09-25 02:20:35 +1000

    get average height

--- terrain/terrain.rb
+++ terrain/terrain.rb
@@ -7,20 +7,34 @@ class Terrain
 
   def read_data (data_in)# loads data and puts it into the plot_cord variable
     input = File.open(data_in, File::RDONLY){|f| f.read }
-    @plot_cord =  input.lines.map{|l| l.split.map(&:to_i) }#put cord into array
+    @plot_cord =  input.lines.map{|l| l.split.map(&:to_f) }#put cord into array
     @plot_size = @plot_cord[0][0]#get the size
     @plot_cord.shift#remove size from array to leave only cord
-    plot_points
+    #plot_points
+    get_ave_height
   end
   def plot_points()
-    count = 0
+    x_count = 0
     @plot_cord.each do |val|
+      y_count = 0
        val.each {|cord|
 
        }
-      p count
-      count +=1
+      p x_count
+       x_count +=1
     end
+  end
+
+  def get_ave_height
+    ave_height = 0
+    num_of_point = 0
+    @plot_cord.each{|val|
+      val.each {|height|
+        ave_height = ave_height + height# add the value of all points
+        num_of_point +=1# add the number of points
+      }
+    }
+    puts ave_height/num_of_point# just for debug
   end
 
 end
