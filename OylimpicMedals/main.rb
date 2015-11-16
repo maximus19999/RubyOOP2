2b2f2b72ed96441ce55347ec72acf34b09aeea69
Max Anderson <maxwell.anderson@griffithuni.edu.au>
2015-11-16 19:48:51 +0000

    main.rb added

--- OylimpicMedals/main.rb
+++ OylimpicMedals/main.rb
@@ -1,6 +1,6 @@
 require_relative "country"
 require_relative"medal_rankings"
 
-t = MedalRankings.new("medals.txt")
-puts t.average_pop
-puts t.most_golds
+medal_calculations = MedalRankings.new("medals.txt")
+puts "Average population of all countries = "+medal_calculations.average_pop.to_s
+puts "country that won most gold medals = "+medal_calculations.most_golds.to_s
